ce43393743b84ff63bb2ed96168fd0e6e464c5ec
Matteo <mateus.sakata@gmail.com>
2020-07-11 14:15:12 -0500

    Refine Baisc Chat UI

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "cc5949075431e8d0503720de2cf63b5d",
 "/": "cc5949075431e8d0503720de2cf63b5d",
-"main.dart.js": "0262292d8ceae0cc48bed257e759e256",
+"main.dart.js": "38f6a1afde4f23aef6cb72e132e950fb",
 "manifest.json": "681ea76ed2284c1442f2503a890f0c4d"
 };
 
